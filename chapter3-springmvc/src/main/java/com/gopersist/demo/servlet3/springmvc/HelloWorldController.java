905960692e2db02289c9bd21e228ac48afa74a4b
Leo <leo@u3dspace.com>
2015-05-07 12:04:08 +0800

    Add spring MVC demo.

---
+++ chapter3-springmvc/src/main/java/com/gopersist/demo/servlet3/springmvc/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.gopersist.demo.servlet3.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+	@RequestMapping(value={"", "/", "/*"}, method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+}
